1a4b720f42535f30d89e838ef1e0778ae1de5e17
Magesh Kuppan <tkmagesh77@gmail.com>
2020-10-06 10:37:51 +0530

    Update projectsReducer.js

--- my-app/src/reducers/projectsReducer.js
+++ my-app/src/reducers/projectsReducer.js
@@ -2,7 +2,7 @@ const initialState = [];
 
 export default function projectsReducer(currentState = initialState, action){
     if (action.type === 'PROJECTS_LOAD'){
-        return action.payload;
+        return [...action.payload];
     }
     return currentState;
 }
