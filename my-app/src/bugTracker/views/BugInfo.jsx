4b145ee7651e767b6ad6221103a67ea1b25f43b0
Magesh Kuppan <tkmagesh77@gmail.com>
2020-10-06 15:59:37 +0530

    Update BugInfo.jsx

--- my-app/src/bugTracker/views/BugInfo.jsx
+++ my-app/src/bugTracker/views/BugInfo.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 
-const BugInfo = (props) => {
+const BugInfo = () => {
     const { id } = useParams();
     return (
         <h3>Bug Info - [{id}]</h3>
